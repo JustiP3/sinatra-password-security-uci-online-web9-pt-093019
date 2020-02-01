785ff1d599cb55933f37401d0466a585f1fa7acb
Justin Phillips <justinphil02@yahoo.com>
2020-02-01 05:35:18 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -31,7 +31,6 @@ class ApplicationController < Sinatra::Base
 
 	post "/login" do
 	  user = User.find_by(:username => params[:username])
-		binding.pry
 	  if user
 	    redirect "/success"
 	  else
@@ -40,6 +39,7 @@ class ApplicationController < Sinatra::Base
 	end
 
 	get "/success" do
+		binding.pry
 		if logged_in?
 			erb :success
 		else
